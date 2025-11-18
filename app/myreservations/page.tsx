ff9756a72d0e213b288d75fee7b881dbd24fd66d
Jaymei28 <jaymeirey.cortez#msunaawan.edu.ph>
2025-11-18 09:54:55 +0800

    Video 2 - Detail page for chat (static) - CORTEZ, JAYMEI REY

--- app/myreservations/page.tsx
+++ app/myreservations/page.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
-import ContactButton from "@/app/components/ContactButton";
-import PropertyList from "@/app/components/properties/PropertyList";
+
 
 const MyReservationsPage = () => {
     return (
@@ -8,7 +7,7 @@ const MyReservationsPage = () => {
                 <h1 className="mt-6 pb-2 text-2xl">My Reservations</h1>
 
                 <div className=" space-y-4">
-                    <div className="p-5  grid grid-cols-1 md:grid-cols-4 gap-4 shadow-md border border-gray-300 border-rounded rounded-xl">
+                    <div className="p-5  grid grid-cols-1 md:grid-cols-4 gap-4 shadow-md border border-gray-300 rounded-xl">
                         <div className="col-span-1">
                             <div className="relative overflow-hidden aspect-square rounded-xl">
                                 <Image
@@ -33,14 +32,14 @@ const MyReservationsPage = () => {
                         </div>
                     </div>
 
-                    <div className="p-5  grid grid-cols-1 md:grid-cols-4 gap-4 shadow-md border border-gray-300 border-rounded rounded-xl">
+                    <div className="p-5  grid grid-cols-1 md:grid-cols-4 gap-4 shadow-md border border-gray-300 rounded-xl">
                         <div className="col-span-1">
                             <div className="relative overflow-hidden aspect-square rounded-xl">
                                 <Image
                                     fill
                                     src="/beach_1.jpg"
                                     alt="Beach house"
-                                    className="hover:scale-110 obeject-cover transition h-full w-full"
+                                    className="hover:scale-110 object-cover transition h-full w-full"
                                 />
                             </div>
                         </div>
