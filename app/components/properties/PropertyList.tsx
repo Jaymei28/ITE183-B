df483cc5a64bc74d81df195a80923d0fdfa091c7
Jaymei28 <jaymeirey.cortez@msunaawan.edu.ph>
2025-11-22 22:38:56 +0800

    Video 3 - Get properties in the front end and list them - CORTEZ, JAYMEI REY

--- app/components/properties/PropertyList.tsx
+++ app/components/properties/PropertyList.tsx
@@ -1,11 +1,53 @@
+'use client';
+
+import { useEffect, useState } from "react";
 import PropertyListItem from "./PropertyListItem";
+import { get } from "http";
+
+export type PropertyType = {
+    id: string;
+    title: string;
+    image_url: string;
+    price_per_night: number;
+}
+
 
 const PropertyList = () => {
+    const [properties, setProperties] = useState<PropertyType[]>([]);
+
+    const getProperties = async () => {
+        const url = 'http://localhost:8000/api/properties/';
+
+        await fetch(url, {
+            method: 'GET',
+        })    
+
+        .then((response) => response.json())
+        .then((json) => {
+            console.log('json', json);
+
+            setProperties(json.data);
+        })
+        .catch((error) => {
+            console.error('error', error);
+        });
+    };  
+
+        useEffect(() => {
+                getProperties();
+            }, []);
+
     return (
             <>
-                <PropertyListItem/>
-                <PropertyListItem/>
-                <PropertyListItem/>
+            {properties.map((property: any) => {
+                return (
+                <PropertyListItem
+                    key={property.id}
+                    property={property}
+                    
+                />
+                );
+            })}
             </>
     );
 };  
