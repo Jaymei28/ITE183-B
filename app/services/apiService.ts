460f918a3ae870f63f9b82bf4097221abf8e0f2e
Jaymei28 <jaymeirey.cortez@msunaawan.edu.ph>
2025-11-25 23:12:00 +0800

    Video 4 - Sign Up - CORTEZ, JAYMEI REY

--- app/services/apiService.ts
+++ app/services/apiService.ts
@@ -18,11 +18,35 @@ const apiService = {
           resolve(json);
         })
         .catch((error) => {
-          console.error('Error:', error);
           reject(error);
         });
     });
   },
+
+  post: async function(url: string, data: any): Promise<any> {
+    console.log('post', url, data);
+
+    return new Promise((resolve, reject) => {
+      fetch(`${API_HOST}${url}`, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+       
+      })
+        .then((response) => response.json())
+        .then((json) => {
+          console.log('Response:', json);
+          resolve(json);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  },
+
 };
 
 export default apiService;
