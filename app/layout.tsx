3974bbd6eaf142709abdbdba9222ef4870137925
Jaymei28 <jaymeirey.cortez@msunaawan.edu.ph>
2025-12-14 21:09:24 +0800

    Video 8 - Modal for Search Filter - CORTEZ, JAYMEI REY

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import Modal from "./components/modals/Modal";
 import LoginModal from "./components/modals/LoginModal";
 import SignupModal from "./components/modals/SignupModal";
 import AddPropertyModal from "./components/modals/AddPropertyModal";
+import SearchModal from "./components/modals/SearchModal";
 
 
 
@@ -32,6 +33,7 @@ export default function RootLayout({
         </div>
 
         <LoginModal />
+        <SearchModal />
         <SignupModal />
         <AddPropertyModal />
       </body>
